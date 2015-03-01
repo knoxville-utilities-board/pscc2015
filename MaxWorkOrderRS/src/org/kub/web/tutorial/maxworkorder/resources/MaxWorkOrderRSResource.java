138157ad7a0c84e7057ff649a6139e0bf9a2de00
fdlane <fdlane61@gmail.com>
2015-03-01 16:26:29 -0500

    fixed null pointer when no paging data was sent.

--- MaxWorkOrderRS/src/org/kub/web/tutorial/maxworkorder/resources/MaxWorkOrderRSResource.java
+++ MaxWorkOrderRS/src/org/kub/web/tutorial/maxworkorder/resources/MaxWorkOrderRSResource.java
@@ -30,137 +30,138 @@ import com.ibm.json.java.JSONObject;
 @Path("workorder")
 public class MaxWorkOrderRSResource {
 
-    @POST
-    @Path("getList")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getList(@HeaderParam(value = "Range") String rangeHeader,
-            @QueryParam("filter") List<String> filters,
-            @QueryParam("sort") List<String> sorts) throws KUBGenericException {
-        List<Filter> filterList = null;
-        List<Sort> sortList = null;
+	@POST
+	@Path("getList")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getList(@HeaderParam(value = "Range") String rangeHeader,
+			@QueryParam("filter") List<String> filters,
+			@QueryParam("sort") List<String> sorts) throws KUBGenericException {
+		List<Filter> filterList = null;
+		List<Sort> sortList = null;
 
-        // Process incoming filters
-        try {
-            filterList = FilterUtils.getFiltersFromQueryParameterList(filters);
-        } catch (Exception e) {
-            throw new KUBGenericException(e.getMessage(), "filterException");
-        }
+		// Process incoming filters
+		try {
+			filterList = FilterUtils.getFiltersFromQueryParameterList(filters);
+		} catch (Exception e) {
+			throw new KUBGenericException(e.getMessage(), "filterException");
+		}
 
-        // Process incoming sorts
-        try {
-            sortList = SortUtils.getSortsFromQueryParameterList(sorts);
-        } catch (Exception e) {
-            throw new KUBGenericException(e.getMessage(), "sortException");
-        }
+		// Process incoming sorts
+		try {
+			sortList = SortUtils.getSortsFromQueryParameterList(sorts);
+		} catch (Exception e) {
+			throw new KUBGenericException(e.getMessage(), "sortException");
+		}
 
-        // Process incoming paging
-        Page page = null;
-        if (rangeHeader != null) {
-            // parses the rangeHeader into a usable Range object
-            Range range = new Range(rangeHeader);
-            page = new Page(range);
-        }
+		// Process incoming paging
+		Page page = null;
+		if (rangeHeader != null) {
+			// parses the rangeHeader into a usable Range object
+			Range range = new Range(rangeHeader);
+			page = new Page(range);
+		}
 
-        SimpleDateFormat dateFormatter = new SimpleDateFormat();
+		SimpleDateFormat dateFormatter = new SimpleDateFormat();
 
-        JSONArray workOrders = new JSONArray();
+		JSONArray workOrders = new JSONArray();
 
-        // paramsDebug is just to demo what came in with the request
-        JSONObject paramsDebug = new JSONObject();
-        paramsDebug.put("rangeHeader", rangeHeader);
-        paramsDebug.put("filterCount", filterList.size());
-        paramsDebug.put("sortCount", sortList.size());
-        paramsDebug.put("page", page.getStart());
+		// paramsDebug is just to demo what came in with the request
+		JSONObject paramsDebug = new JSONObject();
+		paramsDebug.put("rangeHeader", rangeHeader);
+		paramsDebug.put("filterCount", filterList.size());
+		paramsDebug.put("sortCount", sortList.size());
+		if (null != page) {
+			paramsDebug.put("page", page.getStart());
+		}
+		// Some fake Work Orders
+		for (int i = 0; i < 4; i++) {
 
-        // Some fake Work Orders
-        for (int i = 0; i < 4; i++) {
+			JSONObject workOrder = new JSONObject();
 
-            JSONObject workOrder = new JSONObject();
+			// the fake work order
+			workOrder.put("id", 100 + i);
+			workOrder.put("workOrder", "WO-" + (100 + i));
+			workOrder.put("status", "pending");
+			workOrder.put("location", "42 Wallaby Way");
+			workOrder.put("startDate", dateFormatter.format(new Date()));
 
-            // the fake work order
-            workOrder.put("id", 100 + i);
-            workOrder.put("workOrder", "WO-" + (100 + i));
-            workOrder.put("status", "pending");
-            workOrder.put("location", "42 Wallaby Way");
-            workOrder.put("startDate", dateFormatter.format(new Date()));
+			// just for proofing of params
+			workOrder.put("debugging", paramsDebug);
 
-            // just for proofing of params
-            workOrder.put("debugging", paramsDebug);
+			workOrders.add(workOrder);
+		}
 
-            workOrders.add(workOrder);
-        }
+		return Response.ok(new KUBDataObject(workOrders))
+				.header(Range.CONTENT_RANGE_HEADER, "items 0-4/4").build();
+	}
 
-        return Response.ok(new KUBDataObject(workOrders))
-                .header(Range.CONTENT_RANGE_HEADER, "items 0-4/4").build();
-    }
+	@POST
+	@Path("getById/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getById(@PathParam("id") String id) {
 
-    @POST
-    @Path("getById/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(@PathParam("id") String id) {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat();
 
-        SimpleDateFormat dateFormatter = new SimpleDateFormat();
+		String idProof = id;
 
-        String idProof = id;
+		// demo 404 response when item not found
+		if (idProof.equals("bogus")) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
 
-        // demo 404 response when item not found
-        if (idProof.equals("bogus")) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
+		// fake Work Order
+		JSONObject workOrder = new JSONObject();
+		workOrder.put("id", idProof + " -- from path param. Woot!");
+		workOrder.put("workOrder", "WO-3871");
+		workOrder.put("status", "complete");
+		workOrder.put("location", "742 Evergreen Terrace");
+		workOrder.put("startDate", dateFormatter.format(new Date()));
 
-        // fake Work Order
-        JSONObject workOrder = new JSONObject();
-        workOrder.put("id", idProof + " -- from path param. Woot!");
-        workOrder.put("workOrder", "WO-3871");
-        workOrder.put("status", "complete");
-        workOrder.put("location", "742 Evergreen Terrace");
-        workOrder.put("startDate", dateFormatter.format(new Date()));
+		return Response.ok(workOrder).build();
+	}
 
-        return Response.ok(workOrder).build();
-    }
+	@POST
+	@Path("create")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response create(JSONObject workOrder) throws KUBGenericException {
 
-    @POST
-    @Path("create")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response create(JSONObject workOrder) throws KUBGenericException {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat();
 
-        SimpleDateFormat dateFormatter = new SimpleDateFormat();
+		// New fake Work Order
+		Random randomGenerator = new Random();
+		int randomInt = randomGenerator.nextInt(100);
 
-        // New fake Work Order
-        Random randomGenerator = new Random();
-        int randomInt = randomGenerator.nextInt(100);
+		workOrder.put("id", randomInt);
+		workOrder.put("workOrder", "W12-" + randomInt);
+		workOrder.put("status", "new");
+		workOrder.put("location", workOrder.get("location"));
+		workOrder.put("startDate", dateFormatter.format(new Date()));
 
-        workOrder.put("id", randomInt);
-        workOrder.put("workOrder", "W12-" + randomInt);
-        workOrder.put("status", "new");
-        workOrder.put("location", workOrder.get("location"));
-        workOrder.put("startDate", dateFormatter.format(new Date()));
+		return Response.ok(new KUBDataObject(workOrder)).build();
+	}
 
-        return Response.ok(new KUBDataObject(workOrder)).build();
-    }
+	@POST
+	@Path("update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response update(JSONObject workOrder) throws KUBGenericException {
 
-    @POST
-    @Path("update")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response update(JSONObject workOrder) throws KUBGenericException {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat();
 
-        SimpleDateFormat dateFormatter = new SimpleDateFormat();
+		workOrder.put("updatedDateTime", dateFormatter.format(new Date()));
 
-        workOrder.put("updatedDateTime", dateFormatter.format(new Date()));
+		return Response.ok(new KUBDataObject(workOrder)).build();
+	}
 
-        return Response.ok(new KUBDataObject(workOrder)).build();
-    }
+	@POST
+	@Path("remove/{id}")
+	public Response remove(@PathParam("id") String id)
+			throws KUBGenericException {
 
-    @POST
-    @Path("remove/{id}")
-    public Response remove(@PathParam("id") String id)
-            throws KUBGenericException {
-    	
-    	//just to demo PathParams
-    	System.out.println("deleted work order:  " + id);
-    	
-        return Response.status(501).build();
-    }
+		// just to demo PathParams
+		System.out.println("deleted work order:  " + id);
+
+		return Response.status(501).build();
+	}
 }
