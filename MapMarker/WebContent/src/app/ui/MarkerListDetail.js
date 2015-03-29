fafa1db8d58dd54ae7d5e9d0f65344eba7e7cf6e
ebrox <spyridar@gmail.com>
2015-03-29 18:57:25 -0400

    Added Filtering via Categories Dropdown. Also added Sorting button based on created date and a checkbox to add a filter for active markers.

--- MapMarker/WebContent/src/app/ui/MarkerListDetail.js
+++ MapMarker/WebContent/src/app/ui/MarkerListDetail.js
@@ -2,16 +2,24 @@ define(["dojo/_base/declare",
     "dojo/_base/lang",
     "common/routing/router",
     "common/ui/Button",
+    "common/ui/CheckBox",
     "common/ui/StoreList",
     "common/ui/ListDetailView",
+    "common/ui/DropdownStoreList",
+    "app/ui/DropdownListItem",
     "app/ui/Marker",
     "app/ui/MarkerListItem"],
 
-function(declare, lang, router, Button, StoreList, ListDetailView, Marker, MarkerListItem) {
+function(declare, lang, router, Button, CheckBox, StoreList, ListDetailView, DropdownStoreList, DropdownListItem, Marker, MarkerListItem) {
 
     return declare([ListDetailView], {
         postCreate: function() {
             console.log("postCreate()");
+            
+            // Ed Broxson 3-29-15
+            //  Added flags for sorting and filtering
+            var direct = null;
+            var active = null;
 
             var store = this.store = lang.getObject("marker.stores.markers", false, app);
 
@@ -19,6 +27,11 @@ function(declare, lang, router, Button, StoreList, ListDetailView, Marker, Marke
                 store: store,
                 select: "single",
                 hasWidgetContainer: true,
+                // Ed Broxson 3-29-15
+                //  Added query for sorting and filtering
+                query: {
+                	isActive: true
+                },
                 itemRenderer: MarkerListItem,
                 onSelect: function(evt) {
                     router.go("/marker/" + this.item.id);
@@ -26,20 +39,71 @@ function(declare, lang, router, Button, StoreList, ListDetailView, Marker, Marke
             });
             this.set("list", list);
             
-            var queryButton = this.queryButton = new Button({
-            	innerHTML: '<i class="buttonIcon fa fa-2x fa-minus"></i>',
-            	"class": "pull-left btn-link userListWidget",
-            	title: "Filter Markers"
+            // Ed Broxson 3-29-15
+            //  Added catStore and catDropdown for sorting and filtering
+            var catStore = lang.getObject("marker.stores.categories", false, app);
+            var catDropdown = new DropdownStoreList({
+            	store: catStore,
+            	select: "single",
+            	itemRenderer: DropdownListItem
             });
-            queryButton.on("click", lang.hitch(this, function(){
+            
+            catDropdown.on("select", lang.hitch(this, function(){
             	list.clearList();
             	list.setQuery({
-            		createdBy: "fsdarwin"
+            		categoryId: catDropdown.get("value"),
+            		isActive: active
             	}, {
-            		sort: [{attribute: "createdDate", descending: true}]
+            		sort: [{attribute: "createdDate", descending: direct}]
             	});
             }));
-            list.addWidget(queryButton);
+            list.addWidget(catDropdown);
+            
+            // Ed Broxson 3-29-15
+            //  Added isActiveCheckbox for sorting and filtering
+            var isActiveCheckbox = this.isActiveCheckbox = new CheckBox({
+            	// Next 2 lines will change the checkbox graphic to be more inline with others on page
+            	//  but it will not be obvious when checked
+//            	innerHTML: '<i class="checkBoxIcon fa fa-2x fa-check-square"></i>',
+//            	"class": "fa.pull-right btn-link",
+            	label: "Inactive"
+            });
+            isActiveCheckbox.on("click", lang.hitch(this, function(){
+            	if(active == null){
+            		active = true;
+            	}
+            	active = !active;
+            	list.clearList();
+            	list.setQuery({
+            		categoryId: catDropdown.get("value"),
+            		isActive: active
+            	}, {
+            		sort: [{attribute: "createdDate", descending: direct}]
+            	});
+            }));
+            list.addWidget(isActiveCheckbox);
+            
+            // Ed Broxson 3-29-15
+            //  Changed from queryButton and modified for sorting and filtering
+            var sortButton = this.sortButton = new Button({
+            	innerHTML: '<i class="buttonIcon fa fa-2x fa-toggle-down"></i>',
+            	"class": "pull-left btn-link userListWidget",
+            	title: "Sort Markers"
+            });
+            sortButton.on("click", lang.hitch(this, function(){
+            	if(direct == null){
+            		direct = false;
+            	}
+            	direct = !direct;
+            	list.clearList();
+            	list.setQuery({
+            		categoryId: catDropdown.get("value"),
+            		isActive: active
+            	}, {
+            		sort: [{attribute: "createdDate", descending: direct}]
+            	});
+            }));
+            list.addWidget(sortButton);
 
             var createButton = this.createButton = new Button({
                 innerHTML: '<i class="buttonIcon fa fa-2x fa-plus"></i>',
